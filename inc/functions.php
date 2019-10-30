89f1d0b6a0c47f37c914c591e3007997a5b57b3c
lo1cgsan <lo1cgsan@gmail.com>
2019-10-30 09:37:26 +0100

    poprawka generowania tytułu

--- inc/functions.php
+++ inc/functions.php
@@ -6,9 +6,12 @@
 // 	'klasa' => 'Klasy'
 // );
 
+// github.com/lo1cgsan/phpapp
+// tablica asocjcyjna, która będzie zawierała wyniki zapytań
+$ret = array();
+
 function get_menu($id) {
-	global $db;
-	$ret = array();
+	global $db, $ret;
 	db_query('SELECT * FROM menu', $ret);
 	//print_r($ret);
  	foreach ($ret as $k => $t) {
@@ -21,11 +24,16 @@ function get_menu($id) {
 }
 
 function get_page_title($id) {
-	global $pages;
-	if (array_key_exists($id, $pages))
-		echo $pages[$id];
-	else
-		echo 'Aplikacja PHP';
+	global $ret;
+	foreach ($ret as $k => $t) {
+		//echo $t['id']." ";
+		if ($t['plik'] == $id) {
+			echo $t['tytul'];
+			return;
+		}
+	}
+	// tytuł domyślny
+	echo 'Aplikacja PHP';
 }
 
 function get_page_content($id) {
